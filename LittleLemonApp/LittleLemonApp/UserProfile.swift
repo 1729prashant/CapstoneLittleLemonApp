7ea4fb308c69e8dba047e92a5f1d273899930b19
Prashant V Gaikar <1729prashant@gmail.com>
2024-09-28 00:32:15 +0530

    Updated user profile logic

--- LittleLemonApp/LittleLemonApp/UserProfile.swift
+++ LittleLemonApp/LittleLemonApp/UserProfile.swift
@@ -18,21 +18,67 @@ struct UserProfile: View {
     var body: some View {
         VStack {
             Text("Personal information")
+                .font(.title2)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+            
+            
+            
+            
             //add image from assets
             Image("profile-image-placeholder")
             Text(firstName)
+                
             Text(lastName)
             Text(email)
             
-            
+            Spacer()
             NavigationLink {
                 Onboarding()
             } label: {
-                Text("Logout")
+                Text("Log out")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.highlightColor2)
+                    .frame(width: 352, height: 44)
+                    .background(Color.primaryColor2)
+                    .cornerRadius(8)
             }
             .onTapGesture {
                 UserDefaults.standard.set(false, forKey: "kIsLoggedIn")
                 self.presentation.wrappedValue.dismiss()
+            }
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("Discard changes")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.highlightColor2)
+                        .frame(width: 150, height: 44)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.primaryColor1, lineWidth: 4)
+                        )
+                        .cornerRadius(8)
+                }
+                .padding(.trailing)
+                
+                
+                Button {
+                    
+                } label: {
+                    Text("Save changes")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 150, height: 44)
+                        .background(Color.primaryColor1)
+                        .cornerRadius(8)
+                }
+                .padding(.leading)
+                
             }
         }
     }
@@ -41,3 +87,7 @@ struct UserProfile: View {
 #Preview {
     UserProfile()
 }
+
+
+
+
